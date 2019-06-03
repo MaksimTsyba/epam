5ae6a5238f73c0f22ae9eb7b863130da383c40d3
max <MaksimTsuba@gmail.com>
2019-06-03 18:02:38 +0300

    parse expression by brackets separated code to some functions

--- test123.py
+++ test123.py
@@ -4,7 +4,11 @@ import builtins
 
 
 def calc_math(value):
-    return getattr(math, value[0])(float(value[1]))
+    if value[0] == 'pi' or value[0] == 'e':
+        result = getattr(math, value[0])
+    else:
+        result = getattr(math, value[0])(float(value[1]))
+    return result
 
 
 def cal_additional_parameters(value):
@@ -16,13 +20,75 @@ additional_param = ['abs', 'round']
 all_parameters = math_param + additional_param
 
 
-def pcalc(value):
-    left_bracket = re.findall(r'\(', value)
-    right_bracket = re.findall(r'\)', value)
-    if len(left_bracket) != len(right_bracket):
-        return 'error: missing bracket'
+# def pcalc(value):
+#     left_bracket = re.findall(r'\(', value)
+#     right_bracket = re.findall(r'\)', value)
+#     if len(left_bracket) != len(right_bracket):
+#         return 'error: missing bracket'
+#
+#     math_parameters = re.findall(r'(\w+)\s*\((.+?)\)', value)
+#     print(math_parameters)
+#     value = value.replace('pi', str(math.pi)).replace('e', str(math.e))
+#
+#     value = value.replace(' (', '(')
+#     for items in math_parameters:
+#         if items[0] not in all_parameters:
+#             return 'Error: parameter "' + items[0] + '" not found'
+#         if items[0] in additional_param:
+#             value = value.replace(items[0] + '(' + items[1] + ')', str(cal_additional_parameters(items)))
+#         else:
+#             value = value.replace(items[0] + '(' + items[1] + ')', str(calc_math(items)))
+#
+#     print(value)
+#
+#     print(len(left_bracket))
+#     if len(left_bracket) or len(right_bracket):
+#         x = []
+#         z = ""
+#         counter = False
+#         for item in value:
+#
+#             if item == '(' or item == ')':
+#                 if item == ')':
+#                     z += item
+#                 if len(z) > 0:
+#                     if z.find('(') != -1 and z.find(')') != -1:
+#                         z.replace('(', '').replace(')', '')
+#                         if len(x) > 0:
+#                             if x[-1].find('(') != -1:
+#                                 z = x[-1] + str(eval(z))
+#                                 del x[-1]
+#                             else:
+#                                 z = str(eval(z))
+#                                 x.append(z)
+#                                 z = ""
+#                             counter = True
+#                         else:
+#                             z = str(eval(z))
+#                             x.append(z)
+#                     else:
+#                         x.append(z)
+#                         z = ""
+#             z += item
+#             if item == ')' and not counter:
+#                 z = ""
+#             elif counter:
+#                 z = z[:-1]
+#                 counter = False
+#         print(x)
+#         result = eval(''.join(x))
+#
+#     else:
+#         result = eval(value)
+#     print('eval_result:', eval(value))
+#     print('my_result:', result)
+#     return result
 
+
+def calculate_math_parameters(value):
     math_parameters = re.findall(r'(\w+)\s*\((.+?)\)', value)
+    print(math_parameters)
+    value = value.replace('pi', str(math.pi)).replace('e', str(math.e))
 
     value = value.replace(' (', '(')
     for items in math_parameters:
@@ -33,56 +99,78 @@ def pcalc(value):
         else:
             value = value.replace(items[0] + '(' + items[1] + ')', str(calc_math(items)))
 
-    print(value)
-    print(eval(value))
+    return value
 
-    counter = 0
+
+def parse_brackets(value):
     x = []
     z = ""
-    bracket = False
-    test = 0
+    counter = False
+    count_value = 1
+
     for item in value:
-        if item == '(' and not bracket:
-            if test == 0:
-                x.append(z)
-                z = ""
-            else:
-                x[-1] = z
-                z = ""
-            # counter = 0
-
-        if item == '(' and len(z) > 0:
-            bracket = True
-
-        z += item
-
-        if item == ')' and not bracket:
-
-            if z.find('(') != -1 and z.find(')') != -1:
-
-                if counter == -1:
-                    z = str(eval(z))
-                    counter = 0
+        if item == '(' or item == ')':
+            if item == ')':
+                z += item
+            if len(z) > 0:
+                if z.find('(') != -1 and z.find(')') != -1:
+                    z.replace('(', '').replace(')', '')
+                    if len(x) > 0:
+                        if x[-1].find('(') != -1:
+                            z = x[-1] + str(eval(z))
+                            del x[-1]
+                        else:
+                            z = str(eval(z))
+                            x.append(z)
+                            z = ""
+                        counter = True
+                    else:
+                        z = str(eval(z))
+                        x.append(z)
                 else:
-                    z = x[-1] + str(eval(z))
-                    counter = -1
-                x[-1] = z
-                test = 1
-            else:
+                    x.append(z)
+                    z = ""
+        else:
+            if len(value) == count_value:
+                z += item
                 x.append(z)
-                z = ""
-                counter = 0
-                test = 0
-
-        elif item == ')' and bracket:
-            bracket = False
-
-    print(x)
-    result = value
-
+        z += item
+        if item == ')' and not counter:
+            z = ""
+        elif counter:
+            z = z[:-1]
+            counter = False
+        count_value += 1
+    result = eval(''.join(x))
     return result
 
 
-pcalc('(13 + 4) - (log10(5) + ( 3 + round(7.5) + 5)) + (log10(4) + 4)')
-# pcalc('10 + (log10(5) + ( 3 + round(7.5) + 5) + 10)')
-# print(math.log(5)+ 7)
+def pcalc2(value):
+    left_bracket = re.findall(r'\(', value)
+    right_bracket = re.findall(r'\)', value)
+    if len(left_bracket) != len(right_bracket):
+        return 'error: missing bracket'
+
+    result_calculate = calculate_math_parameters(value)
+
+    print(result_calculate)
+    if len(left_bracket) or len(right_bracket):
+        result = parse_brackets(result_calculate)
+    else:
+        result = eval(value)
+    print('eval_result:', eval(value))
+    print('my_result:', result)
+    return result
+
+
+# pcalc('(10+4)-(log10(5) + ( 3 + round(7.5) + 5) + 10) + (log10(4) + 4)')
+# pcalc('10 + (log10(5) + ( 3 + abs(7.5) + 5) + 10)')
+# pcalc('(10 + 1) + (9 + (log(3) + pi) + 10)+(11+4)')
+# pcalc('sin(e**log(e**e**sin(23.0),45.0) + cos(3.0+log10(e**-e)))')
+# pcalc('sin(e**log(e**e**sin(23.0),45.0) + cos(3.0+log10(e**-e)))')
+# print(4 >= 3)
+# print(eval('sin(e**log(e**e**sin(23.0),45.0) + cos(3.0+log10(e**-e)))'))
+
+
+print(pcalc2('(3+4) * 44'))
+# print()
